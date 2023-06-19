674d26dae7c84d36e8cfd58b3043d9252f9a76c9
Spaceworm12 <127404549+Spaceworm12@users.noreply.github.com>
2023-06-19 21:28:23 +0300

    its works

--- app/src/main/java/com/example/homework/MainActivity.kt
+++ app/src/main/java/com/example/homework/MainActivity.kt
@@ -1,16 +1,35 @@
 package com.example.homework
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.Gravity
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.homework.databinding.ActivityMainBinding
 import com.example.homework.presentation.recycler.ListFragment
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.concurrent.TimeUnit
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
 
+    @SuppressLint("CheckResult", "SimpleDateFormat")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val calendar: Calendar = Calendar.getInstance()
+        val df = SimpleDateFormat("HH:mm:ss")
+        val formattedDate: String = df.format(calendar.time)
+        Observable.interval(5000L, TimeUnit.MILLISECONDS)
+            .timeInterval()
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe { Toast.makeText(this, formattedDate,Toast.LENGTH_SHORT).show() }
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
