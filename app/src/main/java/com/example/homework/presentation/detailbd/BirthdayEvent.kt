dcdbdf1c81d00baa37ce4501f5804955fd993a52
Viktor Radchenko <spaceworm1@yandex.ru>
2023-07-27 12:45:11 +0300

    compose sec

--- app/src/main/java/com/example/homework/presentation/detailbd/BirthdayEvent.kt
+++ app/src/main/java/com/example/homework/presentation/detailbd/BirthdayEvent.kt
@@ -4,7 +4,6 @@ import com.example.homework.presentation.model.NoteModel
 import com.example.homework.presentation.recycler.ListEvents
 
 sealed class BirthdayEvent {
-    class ShowCalendar(val itsShow: Boolean) : BirthdayEvent()
     class SetBirthdayNote(val note: NoteModel) : BirthdayEvent()
     class SaveBirthdayNote(val id: Long) : BirthdayEvent()
 
